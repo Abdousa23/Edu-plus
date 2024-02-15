46ec939f0c6edb606bc05ee23821ccf12b5ab0f2
abdou <a_sahki@estin.dz>
2024-02-15 21:28:27 +0100

    initialization database

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,19 @@ const connectDB = require('./config/connectDB');
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const router = require("./routes/index")
+const cookieParser = require('cookie-parser')
 dotenv.config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to my API');
+});
+
+app.use('/api', router);
 
 
 connectDB()
