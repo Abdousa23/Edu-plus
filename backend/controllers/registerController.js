8e8a8e253007b03bbd7e4ceddf26a08d25d9f09e
abdou <a_sahki@estin.dz>
2024-02-15 22:12:23 +0100

    register new user

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -3,26 +3,29 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
-    const {username,email,password}=req.body
-    if(!username || !email || !password){
+    const {firstname,lastname,username,email,password,role,phonenumber}=req.body
+    if(!username || !email || !password || !role || !firstname || !lastname){
         return res.status(400).json("All fields are required")
     }
     // const duplicate = User.find({email:email}||{username:username})
     const duplicate = await User.find({ $or: [{email: email}, {username: username}] });
-
-    console.log(duplicate)
     if(duplicate.length!==0){
         return res.status(409).json("User already exists")
     }
     try {
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt);
-
+        const roles={}
+        role==="school" ? roles={"school":3000} : roles={"user":2000}
+        const phone = phonenumber ? phonenumber : ""
         const newUser = await new User({
             username: req.body.username,
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
             email: req.body.email,
             password: hashedPass,
-            roles:{"user":2000}
+            roles:roles,
+            phonenumber:phone
         })
 
         const user = await newUser.save();
---
+++ backend/routes/auth.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+router.post('/auth', )
