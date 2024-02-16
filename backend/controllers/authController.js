c4b7e2e1c0761f9da68040a73cb4bf6a5022dc2e
majd <m_zeghdar@estin.dz>
2024-02-16 21:25:00 +0100

    fixing dumb crap

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,69 +4,6 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const handleAuth = async (req,res)=>{
-    // const cookies = req.cookies
-    // const {email,password}=req.body
-    // console.log(cookies)
-    // if(!email || !password){
-    //     return res.status(400).json("All fields are required")
-    // }
-    // try {
-    //     const user = await User.findOne({ email: req.body.email });
-    //     !user && res.status(404).json('User not found');
-    //     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    //     !validPassword && res.status(400).json('Wrong password');
-    //     const roles = Object.values(user.roles)
-    //     console.log(roles)
-    //     console.log(user.roles)
-    //     //JWT
-    //     console.log(user.username)
-    //     const accessToken = jwt.sign(
-    //         {"UserInfo":{
-    //             "username": user.username,
-    //             "roles":roles
-    //         }},
-    //         process.env.ACCESS_TOKEN_SECRET,
-    //         {expiresIn: '30s'}
-    //         )
-    //     const newRefreshToken = jwt.sign(
-    //         {
-    //             "username": user.username,
-    //         },
-    //         process.env.REFRESH_TOKEN_SECRET,
-    //         {expiresIn: '1d'}
-    //         )
-    //         console.log("ddddd")
-    //     console.log(user.refreshToken)
-    //     let newRefreshTokenArray =
-    //         !cookies?.jwt 
-    //             ? user.refreshToken
-    //             : user.refreshToken.filter((token)=>token!==cookies.jwt)
-    //     console.log("sss")
-    //     if (cookies?.jwt){
-    //         const refreshToken = cookies.jwt;
-    //         const foundToken = await User.findOne({ refreshToken }).exec()
-    //         if (!foundToken) {
-    //             console.log('attempted refresh token reuse at login!')
-    //             // clear out ALL previous refresh tokens
-    //             newRefreshTokenArray = [];
-    //         }
-
-    //         res.clearCookie('jwt',{httpOnly: true,sameSite: 'None', secure : true })
-    //     }
-    //     // const otherUsers = await User.find({username:{$ne:user.username}})
-    //     // const currentUser = {...user , refreshToken}
-    //     user.refreshToken = [...newRefreshTokenArray,newRefreshToken]
-    //     await user.save()
-    //     res.cookie('jwt', newRefreshToken, { httpOnly: true , sameSite: 'None', secure : true ,maxAge: 7 * 24 * 60 * 60 * 1000});
-
-    //     console.log(user)
-    //     return res.status(200).json({user,roles,accessToken})
-    //     // res.status(200).json(user);
-    // } catch (error) {
-    //     res.status(500).json(error);
-    // }
-
-
     const cookies = req.cookies;
     console.log(`cookie available at login: ${JSON.stringify(cookies)}`);
     const { email, password } = req.body;
