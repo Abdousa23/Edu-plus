aebe48ddb1c7756631eb1c26c152d0f2db835ffb
unknown <a_belaiouar@estin.dz>
2024-02-16 01:52:52 +0100

    uslless 2

--- backend/routes/addCourse.js
+++ backend/routes/addCourse.js
@@ -1,43 +1,9 @@
-const express = require('express');
+const router = require('express').Router();
 const User = require('../models/course');
-const router = express.Router();
-const mongoose = require('mongoose');
-const Course = require('../models/course');
+const addCourseController= require('../controllers/addCourseController')
 require('dotenv').config();
+router.post('/addCourse',addCourseController)
 
-
-const addCourse = async (req, res) => { 
-    
-    const { title, type, description, schoolname, teacher, chapters, lessons, level , language,categorie, studentEnrolled,   rating, reviews, imageUrl,  price    }=req.body
-    if(!title || !type || !description || !teacher || !chapters || !lessons  || !language ||! categorie ||     !price ){
-        return res.status(400).json("re entrer les donner ")}
-        try {
-        const newCourse = new Course({
-            title:req.body.title,
-            type:req.body.type,
-            description:req.body.description,
-            schoolname:req.body.schoolname,
-            teacher:req.body.teacher,
-            chapters:req.body.chapters, 
-            lessons:req.body.lessons,
-            level:req.body.level , 
-            language:req.body.language,
-            categorie:req.body.categorie, 
-            studentEnrolled:req.body.studentEnrolled, 
-            rating:req.body.rating, 
-            reviews:req.body.reviews, 
-            imageUrl:req.body.imageUrl,  
-            price:req.body.price
-        })
-        const Course = await newCourse.save();
-        res.status(200).json(Course);
-    }  catch(err){
-        console.log(err)
-
-
-    }}
-router.post('/addCourse',addCourse)
-module.exports = router
 
 
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router()
 const register = require('./register') 
 const refreshToken = require('./refresh');
 const auth = require('./auth');
+const addCourse = require('./addCourse')
 
 
 router.use('/login', auth);
 router.use('/logout', logout);
 router.use('/refresh', refreshToken);
 router.use('/register', register);
+router.use('./addCourse.js',addCourse)
 
 module.exports = router
