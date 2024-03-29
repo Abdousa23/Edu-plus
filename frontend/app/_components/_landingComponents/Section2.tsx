80dec899b02312cb00266f96a81dfe28c7122077
cynhlfn <160418929+cynhlfn@users.noreply.github.com>
2024-03-29 17:54:35 +0100

    creating landing page

--- frontend/app/_components/_landingComponents/Section2.tsx
+++ frontend/app/_components/_landingComponents/Section2.tsx
@@ -7,8 +7,12 @@ export default function Section2() {
       <div className=''>
         <h1 className=' font-semibold max-md:text-center text-[28px]'>We Partner With More than 10+ Companies</h1>
       </div>
-      <div>
-        <img src="images/Rectangle353.svg" alt="" />
+      <div className='flex flex-row p-8 max-lg:flex-col items-center'>
+        <div className='p-5'><img src="images/skillshare.svg" alt="" /></div>
+        <div className='p-5'><img src="images/Udemy.svg" alt="" /></div>
+        <div className='p-5'><img src="images/Google.svg" alt="" /></div>
+        <div className='px-5 pt-1'><img src="images/coursera.svg" alt="" /></div>
+        <div className='p-5'><img src="images/inter_des_found.svg" alt="" /></div>
       </div>
     </div>
     </section>
