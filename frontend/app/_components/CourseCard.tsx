43581fa728d8365063fe27c507b99f6f0fb18768
Amaniaiss <127679371+aissamani@users.noreply.github.com>
2024-04-02 02:24:20 +0100

    Update CourseCard.tsx

--- frontend/app/_components/CourseCard.tsx
+++ frontend/app/_components/CourseCard.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function CourseCard() {
   return (
-    <div className='w-[368px] h-[480px] m-2 shadow-md rounded-lg order-0 '>
+     <div className='w-[325px] h-[420px] m-2 shadow-md rounded-lg order-0 '>
     
         <img src="/images/landing.svg" className='w-full h-2/5 ' alt="" />
     
@@ -12,7 +12,7 @@ export default function CourseCard() {
         <h1 className='font-extrabold text-2xl'>Course Title</h1>
         <p className='font-regular text-base text-[#4d4d4d]'>Course description</p>
         <div className='flex justify-between'>
-            <p className=''>price</p>
+           <p className='font-semibold'>price</p>
             <div>
             <Link href={'/'}  className='flex flex-row justify-center items-center mx-1 px-4 py-2 w-[100%] max-sm:w-16 max-sm:h-8 max-sm:text-sm h-11 border border-green+ rounded-lg order-5 self-stretch flex-grow-0 text-base text-green+'>show details</Link>
             </div>
