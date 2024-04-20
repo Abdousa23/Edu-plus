496f7e4dd81ca11e701461b34e8b989c204ddfa8
unknown <a_belaiouar@estin.dz>
2024-04-20 13:46:58 +0100

    admin route and controloers

---
+++ frontend/app/admin/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+import useAuth from '../_hooks/useAuth'
+import React from 'react'
+import Sidebar from '../profile/_components/Sidebar'
+import { useState } from 'react'
+
+export default function Page() {
+  return (
+    <div>
+      
+        <Sidebar/>
+
+    </div>
+
+  )
+}
