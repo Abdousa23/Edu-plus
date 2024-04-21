0053294a1ab6fe9f625e755f4af97ac903fd0112
unknown <a_belaiouar@estin.dz>
2024-04-22 00:19:10 +0100

    fixing the scroll err and the send message to only selected group

--- frontend/context/SocketContext.tsx
+++ frontend/context/SocketContext.tsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }: SocketContextProps) => {
     const [onlineUsers, setOnlineUsers] = useState<any>([]);
     const { auth ,setAuth } = useAuth();
 
-    useEffect(() => {''
+    useEffect(() => {
         if (auth?.user) {
 
             const socket = io('http://localhost:3001', {
