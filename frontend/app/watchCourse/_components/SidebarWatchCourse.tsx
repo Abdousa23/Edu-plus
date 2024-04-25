65b6a247971ea0396778c6f94c337bd1d0aa5e3e
cynhlfn <160418929+cynhlfn@users.noreply.github.com>
2024-04-25 22:43:54 +0100

    lessons Side Bar02

--- frontend/app/watchCourse/_components/SidebarWatchCourse.tsx
+++ frontend/app/watchCourse/_components/SidebarWatchCourse.tsx
@@ -5,10 +5,10 @@ import LessonsCard from './LessonsCard'
 
 export default function SidebarWatchCourse() {
   return (
-    <div>
+    <div className='justify-around sticky  flex flex-col items-center sm:w-full md:w-1/2 lg:w-1/3  top-0 left-0 bg-white shadow-md rounded-[10px] '>
       <div className="flex w-[85%] mx-auto justify-between "> 
         <h1 className='font-bold'>Course Contents</h1>
-        <h1 className='text-[#23BD33]'>15% Completed</h1>
+        <h1 className='text-[#23BD33] text-[14px]'>15% Completed</h1>
       </div>
       <LessonsCard/>
     </div>
