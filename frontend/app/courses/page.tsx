1429da5e6e71d8b3db6989ef3ad8df01babf58f4
abdou <a_sahki@estin.dz>
2024-05-02 17:34:28 +0100

    deploy

--- frontend/app/courses/page.tsx
+++ frontend/app/courses/page.tsx
@@ -1,25 +1,22 @@
 import React from 'react'
-import { useState} from 'react'
 import CourseCard from '../_components/CourseCard';
 export default async function page() {
-  const [error,setError] = useState<string>('');
-  const [courses,setCourses] = useState<CourseType[] | null>(null);
-  try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/home`)
-    const data = await response?.json();
-    setCourses(data);
-  } catch (error) {
-      setError('no courses available ')
-  }
-  return (
-    <div>
-      {
+    const courses = await response?.json();
+    const content =(
+      <div>
+        {
         courses && courses?.length > 0 ? courses?.map((course:CourseType) => {
           return (
             <CourseCard key={course._id} course={course} />
           )
-        }) : <div>{error || 'no course available'}</div>
+        }) : <div>{'no course available'}</div>
       }
-    </div>
+      </div>
+    )
+  return (
+    <>
+    {content}
+    </>
   )
 }
